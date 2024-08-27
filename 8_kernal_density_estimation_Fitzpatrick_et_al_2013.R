65556dac3c1bdfdef6da195acf0268ffc6376eb0
Xin Chen <xch@unc.edu>
2024-08-27 15:47:49 -0400

    Update 8_kernal_density_estimation_Fitzpatrick_et_al_2013.R

--- 8_kernal_density_estimation_Fitzpatrick_et_al_2013.R
+++ 8_kernal_density_estimation_Fitzpatrick_et_al_2013.R
@@ -1,73 +1,56 @@
-################################################################################
-# script to model ant distributions in New England using maxent
-# 
-#
-# Results described in:
-# Fitzpatrick et al. (2013) MAXENT vs. MAXLIKE: Empirical Comparisons with 
-# Ant Species Distributions. Ecosphere
-#
-# DESCRIPTION & NOTES
-# The code needs the following to run:
-# (1) climate & elevation rasters (provided as neClim)
-# (2) ant distribution data (provided as antData_use4R_snappedToClim.csv). Note
-# that as the file name suggests these data have been snapped to cell centriods.
-#
-# Code is provided as is, without support 
-################################################################################
-
-################################################################################
-# CHUNK 1: Read in & prep data
-################################################################################
-library(dismo)
-library(raster)
-library(foreach)
-library(doParallel)
-library(dismo)
-library(raster)
-library(sp)
-library(sm)
-
-setwd("D:/xinchen/mee_sdm_experiment/8_taget_group_backgrounds/Week-7-20240813T221121Z-001/Week-7/data") # setwd to where data files are stored
-
-# read in climate & elev data
-antsGeoXY <- read.csv("antData.csv")
-neClim <- stack("neClim.grd") # three variables
-antsSp <- antsGeoXY
-coordinates(antsSp) <- c("x", "y")
-projection(antsSp) <- projection(neClim)
-
-plot(neClim$bio_1)
-points(antsSp, pch=20, cex=0.5, col=rgb(0,0,0,0.25))
-
-# make a mask raster to use for processing
-mask <- neClim[[1]]>-1000
-
-# To deal with spatial sampling bias, we will create
-# a raster that reflects the sampling density
-# using kernel density estimation (KDE)
-bias <- cellFromXY(mask, antsSp)
-cells <- unique(sort(bias))
-kernelXY <- xyFromCell(mask, cells)
-samps <- as.numeric(table(bias)) 
-# number of samples in each grid cell
-head(samps)
-max(samps) #255!
-
-# code to make KDE raster
-KDEsur <- sm.density(kernelXY, 
-                     #weights=samps, 
-                     display="none", 
-                     ngrid=812, 
-                     ylim=c(40,48), 
-                     xlim=c(-75,-65), 
-                     nbins=0)
-KDErast <- SpatialPoints(expand.grid(x=KDEsur$eval.points[,1], y=KDEsur$eval.points[,2]))
-KDErast <-  SpatialPixelsDataFrame(KDErast, data.frame(kde = array(KDEsur$estimate, 
-                                                                   length(KDEsur$estimate))))
-KDErast <- raster(KDErast)
-plot(KDErast)
-KDErast <- resample(KDErast, mask)
-KDErast <- KDErast*mask
-plot(KDErast)
-KDEpts <- rasterToPoints(KDErast)
-################################################################################
+        # Date: 08/13/2024
+        # Change: 10,000 locations selected using probabilistic target-group sampling based on 
+        # Fitzpatrick et al. (2013) MAXENT vs. MAXLIKE: Empirical Comparisons with 
+        # Ant Species Distributions. Ecosphere
+        set.seed(1)
+        # Try using spatially thinned occurrences
+        temp_occ_i <- st_as_sf(temp_occ, coords = c("x", "y"), na.fail = FALSE, crs = crs(IUCN_all))
+        # Project occurrences from equal area projection to WGS84 
+        temp_occ_i <- st_transform(temp_occ_i, crs(feature_all))
+        
+        # # Try using occurrences before spatial thinning
+        # temp_occ_i <- temp_occ_in_1.1_buffer[temp_occ_feature_i[, 1] %in% na.omit(temp_occ_feature_i)[, 1], ]
+        # temp_occ_i <- st_transform(temp_occ_i, crs = crs(feature_all))
+        mask_i <- raster(paste0("D:/xinchen/mee_sdm_experiment/5_R_script/Output/calibration_raster/", spplist[i], "/bio1.asc"))
+        mask_i <- mask_i > -10000 # Make sure the cell values are all converted to 1
+        temp_occ_i <- sf::as_Spatial(temp_occ_i)
+        
+        # To deal with spatial sampling bias, we will create
+        # a raster that reflects the sampling density
+        # using kernel density estimation (KDE)
+        bias_i <- cellFromXY(mask_i, temp_occ_i)
+        cells_i <- unique(sort(bias_i))
+        kernelXY_i <- xyFromCell(mask_i, cells_i)
+        samps_i <- as.numeric(table(bias_i)) 
+        # number of samples in each grid cell
+        head(samps_i)
+        max(samps_i) #3 is the maximum because we do not have many observations within 1km pixel
+        
+        # code to make KDE raster
+        KDEsur <- sm.density(kernelXY_i, 
+                             #weights=samps, 
+                             display="none", 
+                             ngrid=963, 
+                             ylim=c(14.2598,21.90146), 
+                             xlim=c(-94.42582,-86.40082), 
+                             nbins=0)
+        
+        KDErast <- SpatialPoints(expand.grid(x=KDEsur$eval.points[,1], y=KDEsur$eval.points[,2]))
+        KDErast <-  SpatialPixelsDataFrame(KDErast, data.frame(kde = array(KDEsur$estimate, 
+                                                                           length(KDEsur$estimate))))
+        KDErast <- raster(KDErast)
+        plot(KDErast)
+        KDErast <- resample(KDErast, mask_i)
+        KDErast <- KDErast*mask_i
+        plot(KDErast)
+        KDEpts <- rasterToPoints(KDErast)
+        
+        temp_bg <- randomPoints(KDErast, n = 10000, ext = extent(mask_i), prob = TRUE) %>% as.data.frame()
+        
+        coordinates(temp_bg) <- ~x + y
+        
+        # Define CRS of bg points
+        proj4string(temp_bg) <- CRS("+init=EPSG:4326")
+        
+        # Project bg points to equal area projection the same as IUCN range map
+        temp_bg <- spTransform(temp_bg, crs(IUCN_all))
